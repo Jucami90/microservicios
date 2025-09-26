1011a73ec7cc4fb8ea5f3dc3fca8851bd2e8e2ff
Juan Carlos Miranda Núñez <jucami90@Juans-Mac-mini.local>
2025-09-26 16:04:07 -0500

    add project

---
+++ src/main/java/com/example/demomicroservicios/DemoMicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomicroservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroserviciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMicroserviciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomicroservicios/DemoMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomicroservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
